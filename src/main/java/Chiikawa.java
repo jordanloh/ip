0bd6d043e4fa4b37e10c417effbb5398c27b4ad0
jordanloh <jordan.loh@outlook.com>
2025-08-18 23:59:32 +0800

    Renamed chatbot to Chiikawa, added greetings

---
+++ src/main/java/Chiikawa.java
@@ -0,0 +1,12 @@
+public class Chiikawa {
+    public static void main(String[] args) {
+        String name = "Chiikawa";
+        String divider = "----------------------------------";
+        System.out.println(divider);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
+    }
+}

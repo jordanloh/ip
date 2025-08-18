9c9c0687c30183fc9d2d285aec6857fc66174ab7
jordanloh <jordan.loh@outlook.com>
2025-08-19 00:16:46 +0800

    Updated chatbot to echo commands entered, and exits with 'bye'

--- src/main/java/Chiikawa.java
+++ src/main/java/Chiikawa.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Chiikawa {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String name = "Chiikawa";
         String divider = "----------------------------------";
         System.out.println(divider);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("Hewwo! I'm " + name + "!!");
+        System.out.println("What can I do for you nya~?");
         System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            System.out.println(divider);
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Byebye!! See you again soon nya~!");
+                System.out.println(divider);
+                break;
+            }
+            System.out.println("Awww you said: " + input);
+            System.out.println(divider);
+        }
+        sc.close();
     }
 }

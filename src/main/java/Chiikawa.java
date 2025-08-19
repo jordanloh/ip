600be64bf7e486ea94028b07edc56b88e22f818f
jordanloh <jordan.loh@outlook.com>
2025-08-19 18:46:23 +0800

    Added support for tracking 3 different types of tasks

--- src/main/java/Chiikawa.java
+++ src/main/java/Chiikawa.java
@@ -43,8 +43,22 @@ public class Chiikawa {
                 System.out.println(arr[index].toString());
             }
             else {
-                System.out.println("I've added in: " + input);
-                arr[counter++] = new Task(input);
+                System.out.println("I've added in this task ~nya! :");
+                switch (command) {
+                    case "todo":
+                        arr[counter++] = new Todo(parts[1]);
+                        break;
+                    case "deadline":
+                        String[] deadlineParts = parts[1].split(" /by ", 2);
+                        arr[counter++] = new Deadline(deadlineParts[0], deadlineParts[1]);
+                        break;
+                    case "event":
+                        String[] eventParts = parts[1].split(" /from | /to", 3);
+                        arr[counter++] = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                        break;
+                }
+                System.out.println(arr[counter - 1].toString());
+                System.out.println("Now you have " + counter + " tasks in the list.");
             }
             System.out.println(divider);
         }

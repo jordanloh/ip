c648d801dd512805883867e8f6d5d27c3512980e
jordanloh <jordan.loh@outlook.com>
2025-08-19 17:57:33 +0800

    Added ability to store texts by user, and display them back when requested

--- src/main/java/Chiikawa.java
+++ src/main/java/Chiikawa.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
 public class Chiikawa {
+    private static final String name = "Chiikawa";
+    private static final String divider = "----------------------------------";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = "Chiikawa";
-        String divider = "----------------------------------";
+        String[] arr = new String[100];
+        int counter = 0;
+
         System.out.println(divider);
         System.out.println("Hewwo! I'm " + name + "!!");
         System.out.println("What can I do for you nya~?");
@@ -19,7 +23,15 @@ public class Chiikawa {
                 System.out.println(divider);
                 break;
             }
-            System.out.println("Awww you said: " + input);
+            else if (input.equalsIgnoreCase("list")) {
+                for (int i = 1; i <= counter; i++) {
+                    System.out.println(i + ". " + arr[i-1]);
+                }
+            }
+            else {
+                System.out.println("I've added in: " + input);
+                arr[counter++] = input;
+            }
             System.out.println(divider);
         }
         sc.close();

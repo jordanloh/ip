c29b8a10c2c239da6e34c13b9cabde13baab7209
jordanloh <jordan.loh@outlook.com>
2025-08-19 18:19:00 +0800

    Added functionality to mark and unmark tasks as done

--- src/main/java/Chiikawa.java
+++ src/main/java/Chiikawa.java
@@ -6,7 +6,7 @@ public class Chiikawa {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int counter = 0;
 
         System.out.println(divider);
@@ -15,22 +15,36 @@ public class Chiikawa {
         System.out.println(divider);
 
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
 
             System.out.println(divider);
-            if (input.equalsIgnoreCase("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Byebye!! See you again soon nya~!");
                 System.out.println(divider);
                 break;
             }
-            else if (input.equalsIgnoreCase("list")) {
+            else if (command.equalsIgnoreCase("list")) {
                 for (int i = 1; i <= counter; i++) {
-                    System.out.println(i + ". " + arr[i-1]);
+                    System.out.println(i + ". " + arr[i-1].toString());
                 }
+            }
+            else if (command.equalsIgnoreCase("mark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                arr[index].markAsDone();
+                System.out.println("I've marked this task as done ~nya! : ");
+                System.out.println(arr[index].toString());
+            }
+            else if (command.equalsIgnoreCase("unmark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                arr[index].markAsUndone();
+                System.out.println("I've marked this task as not done yet ~nya! : ");
+                System.out.println(arr[index].toString());
             }
             else {
                 System.out.println("I've added in: " + input);
-                arr[counter++] = input;
+                arr[counter++] = new Task(input);
             }
             System.out.println(divider);
         }

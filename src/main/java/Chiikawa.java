c4802d91abe89997d4ea6ccc7b8bec85d5aff0a5
jordanloh <jordan.loh@outlook.com>
2025-08-19 20:36:42 +0800

    Edited Chiikawa.java to use switch statements for all commands

--- src/main/java/Chiikawa.java
+++ src/main/java/Chiikawa.java
@@ -20,45 +20,52 @@ public class Chiikawa {
             String command = parts[0];
 
             System.out.println(divider);
+
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Byebye!! See you again soon nya~!");
                 System.out.println(divider);
                 break;
             }
-            else if (command.equalsIgnoreCase("list")) {
-                for (int i = 1; i <= counter; i++) {
-                    System.out.println(i + ". " + arr[i-1].toString());
+            switch (command) {
+                case "list":
+                    for (int i = 1; i <= counter; i++) {
+                        System.out.println(i + ". " + arr[i-1].toString());
+                    }
+                    break;
+                case "mark": {
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    arr[index].markAsDone();
+                    System.out.println("I've marked this task as done ~nya! :");
+                    System.out.println(arr[index].toString());
+                    break;
                 }
-            }
-            else if (command.equalsIgnoreCase("mark")) {
-                int index = Integer.parseInt(parts[1]) - 1;
-                arr[index].markAsDone();
-                System.out.println("I've marked this task as done ~nya! :");
-                System.out.println(arr[index].toString());
-            }
-            else if (command.equalsIgnoreCase("unmark")) {
-                int index = Integer.parseInt(parts[1]) - 1;
-                arr[index].markAsUndone();
-                System.out.println("I've marked this task as not done yet ~nya! :");
-                System.out.println(arr[index].toString());
-            }
-            else {
-                System.out.println("I've added in this task ~nya! :");
-                switch (command) {
-                    case "todo":
-                        arr[counter++] = new Todo(parts[1]);
-                        break;
-                    case "deadline":
-                        String[] deadlineParts = parts[1].split(" /by ", 2);
-                        arr[counter++] = new Deadline(deadlineParts[0], deadlineParts[1]);
-                        break;
-                    case "event":
-                        String[] eventParts = parts[1].split(" /from | /to ", 3);
-                        arr[counter++] = new Event(eventParts[0], eventParts[1], eventParts[2]);
-                        break;
+                case "unmark": {
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    arr[index].markAsUndone();
+                    System.out.println("I've marked this task as not done yet ~nya! :");
+                    System.out.println(arr[index].toString());
+                    break;
                 }
-                System.out.println(arr[counter - 1].toString());
-                System.out.println("Now you have " + counter + " tasks in the list.");
+                case "todo":
+                    System.out.println("I've added in this task ~nya! :");
+                    arr[counter++] = new Todo(parts[1]);
+                    System.out.println(arr[counter - 1].toString());
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    break;
+                case "deadline":
+                    System.out.println("I've added in this task ~nya! :");
+                    String[] deadlineParts = parts[1].split(" /by ", 2);
+                    arr[counter++] = new Deadline(deadlineParts[0], deadlineParts[1]);
+                    System.out.println(arr[counter - 1].toString());
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    break;
+                case "event":
+                    System.out.println("I've added in this task ~nya! :");
+                    String[] eventParts = parts[1].split(" /from | /to ", 3);
+                    arr[counter++] = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                    System.out.println(arr[counter - 1].toString());
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    break;
             }
             System.out.println(divider);
         }
